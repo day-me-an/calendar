30ad8750eb72b476543b3873f45314fdf2d27dda
Damian Wood <damian@live.co.uk>
2017-03-08 19:46:20 +0000

    overflow stories

--- src/Grid.stories.js
+++ src/Grid.stories.js
@@ -6,3 +6,12 @@ storiesOf('Grid', module)
   .add('no overflow', () => (
     <Grid days={28} overflowStart={[]} overflowEnd={[]} />
   ))
+  .add('start overflow', () => (
+    <Grid days={30} overflowStart={[27, 28, 29, 30, 31]} overflowEnd={[]} />
+  ))
+  .add('end overflow', () => (
+    <Grid days={31} overflowStart={[]} overflowEnd={[1, 2, 3, 4]} />
+  ))
+  .add('both overflow', () => (
+    <Grid days={31} overflowStart={[27, 28]} overflowEnd={[1, 2]} />
+  ))
