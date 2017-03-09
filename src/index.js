e2b0910002148a89235898cccf182ce24fe37379
Damian Wood <damian@live.co.uk>
2017-03-09 19:50:45 +0000

    routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import {Router, Route, hashHistory, withRouter} from 'react-router'
+
+import {Calendar} from './Calendar'
 import './index.css'
 
+/**
+ * A Calendar that updates the URL when the date is changed.
+ */
+const RoutedCalendar = (props) => {
+  const {year, month} = props.location.query
+  return (
+    <Calendar
+      year={year !== undefined ? parseInt(year, 10) : undefined}
+      month={month !== undefined ? parseInt(month, 10) : undefined}
+      onChange={(year, month) => props.router.replace({query: {year, month}})}
+    />
+  )
+}
+
+const routings = (
+  <Router history={hashHistory}>
+    <Route path="/" component={withRouter(RoutedCalendar)} />
+  </Router>
+)
+
 ReactDOM.render(
-  <App />,
+  routings,
   document.getElementById('root')
 )
